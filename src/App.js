11ad76492a226f92b1b52083638c13816a3ed755
Avinash Singh <mailto:avinashwebappssol@gmail.com>
2023-07-29 13:41:28 +0530

    add function component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import VsjItem from './components/VsjItem';
 
 function App() {
   return (
     <div className="App">
       Avinash
+      <img src='varanasi.jpg'/>
+      <VsjItem />
+      <VsjItem></VsjItem>
          </div>
   );
 }
---
+++ src/components/VsjItem.js
@@ -0,0 +1,11 @@
+// import vsjitem.css
+
+
+function VsjItem(){
+    return <p>
+        <h1>VSJ</h1>
+    
+    </p>
+}
+
+export default VsjItem;
