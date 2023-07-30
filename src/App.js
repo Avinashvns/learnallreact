eafe325e99a2c9915cb6f8af7017fe9739866e8a
Avinash Singh <mailto:avinashwebappssol@gmail.com>
2023-07-30 13:31:06 +0530

    Add Submit Event Handler

--- src/App.js
+++ src/App.js
@@ -5,29 +5,41 @@ import MultipleObject from './components/MultipleObject';
 import VsjFrom from './components/VsjForm';
 // import VsjItem from './components/VsjItem';
 // import VsjWrapper from './components/VsjWrapper';
+import VsjExpenseCalculator from './components/VsjExpenseCalculator';
 
 import EventTarget from './components/EventTarget';
 
 function App() {
   return (
-    <div className="App">
-      {/* Avinash */}
-      {/* <VsjCounter /> */}
-      {/* <img src='varanasi.jpg' alt='kk'/> */}
-      {/* <VsjItem /> */}
-      {/* <VsjItem message='This is a children'></VsjItem> */}
-      {/* <VsjItem message="Of the props"></VsjItem> */}
-      {/* <VsjWrapper>Hello Wrapper</VsjWrapper> */}
+    // <div className="App">
+    //   {/* Avinash */}
+    //   {/* <VsjCounter /> */}
+    //   {/* <img src='varanasi.jpg' alt='kk'/> */}
+    //   {/* <VsjItem /> */}
+    //   {/* <VsjItem message='This is a children'></VsjItem> */}
+    //   {/* <VsjItem message="Of the props"></VsjItem> */}
+    //   {/* <VsjWrapper>Hello Wrapper</VsjWrapper> */}
 
-      {/* <VsjFrom></VsjFrom> */}
-      {/* <h1>Form</h1>
-      <VsjFrom></VsjFrom>
-	    <h1>Multiple</h1>
-	    <MultipleObject></MultipleObject> */}
-      <h1>Target Event</h1>
-      <br/>
-      <EventTarget></EventTarget>
+    //   {/* <VsjFrom></VsjFrom> */}
+    //   {/* <h1>Form</h1>
+    //   <VsjFrom></VsjFrom>
+    //   <h1>Multiple</h1>
+    //   <MultipleObject></MultipleObject> */}
+    //   <h1>Target Event</h1>
+    //   <br/>
+    //   <EventTarget></EventTarget>
+    //   </div>
+    <div class="container pt-5 bg-primary text-white text-center" id="root">
+      <div class="row">
+        <div class="col-lg-1"></div>
+        <div class="col-lg-10 bg-danger">
+          <VsjExpenseCalculator></VsjExpenseCalculator>
+
+        </div>
+        <div class="col-lg-1"></div>
       </div>
+
+    </div>
   );
 }
 
---
+++ src/components/VsjExpenseCalculator.js
@@ -0,0 +1,65 @@
+import React, { useState } from 'react';
+
+const  VsjExpenseCalculator = (props) =>
+{
+	const [
+	expenseData, setExpense] = useState({
+		item:'',
+		amount:0,
+		tdate:''
+		
+		/* Use n object instead of two useState*/
+	});
+	const itemChangeHandler=(event)=>
+	{
+		setExpense( (prevState)=>{
+		return {...prevState,item:event.target.value};
+		
+	});
+	console.log(expenseData);
+	};
+	const amountChangeHandler=(event)=>
+	{
+		setExpense( (prevState)=>{
+		return {...prevState,amount:Number(event.target.value)};
+		
+	});
+	console.log(expenseData);
+	};
+	
+	const dateChangeHandler=(event)=>
+	{
+		setExpense( (prevState)=>{
+		return {...prevState,tdate:new Date(event.target.value)};
+		
+	});
+	console.log(expenseData);
+	};
+	const submitHandler=(event)=>
+	{
+		event.preventDefault();
+		console.log("Submitted");
+        console.log(expenseData);
+		
+	};
+	
+	
+	
+	
+	return  <form onSubmit={submitHandler}>
+  <div class="form-group">
+    <label for="item">Item</label>
+    <input type="text" class="form-control" onChange={itemChangeHandler} placeholder="Enter Item" id="item"/>
+  </div>
+  <div class="form-group">
+    <label for="amount">Amount</label>
+    <input type="number" class="form-control" placeholder="Enter Amount" id="amount" onChange={amountChangeHandler}/>
+  </div>
+  <div class="form-group">
+    <label for="tdate">Date</label>
+    <input type="date" class="form-control" placeholder="Enter Date" id="tdate" onChange={dateChangeHandler}/>
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>;
+}
+export default VsjExpenseCalculator;
